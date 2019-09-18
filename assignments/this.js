2ec71e4e8fe7d36fc118c0f445d3fe2c67a8d926
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-09-18 10:40:52 -0700

    Finished this.js Principal 1

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. Window Binding is the default state of the 'this' keyword, which uses the window object to try to satisfy the statment.
 * 2. 
 * 3. 
 * 4. 
@@ -12,7 +12,10 @@
 // Principle 1
 
 // code example for Window Binding
-
+function howOld(){
+    return this.age;
+}
+console.log(howOld());//undefined
 // Principle 2
 
 // code example for Implicit Binding
