a85dd56854307ee74d01c29e41ba2e543b26e20e
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-09-18 10:42:34 -0700

    Finished this.js Principal 4

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,7 @@
 * 1. Window Binding is the default state of the 'this' keyword, which uses the window object to try to satisfy the statment.
 * 2. Implicit Binding is a binding where the 'this' keyword is implied via dot notation E.G. : adam.sayHi -> adam = this
 * 3. Explicit Binding is when the binding is actively stated, can be done with call, apply, and bind
-* 4. 
+* 4. New Binding is a binding where the scope where 'this' is used is duplicated and 'this' is hardcoded on the new copy
 *
 * write out a code example of each explanation above
 */
@@ -36,3 +36,17 @@ const myPet = new animal('Theona');
 // Principle 4
 
 // code example for Explicit Binding
+function greetPerson(){
+    return `Hello ${this.name}, nice to see you.`;
+}
+const adam = {
+    name: "Adam",
+    gender: 'M'
+}
+const eve = {
+    name: "Eve",
+    gender: 'F'
+}
+console.log(greetPerson.call(adam));
+const setInStone = greetPerson.bind(eve);
+console.log(setInStone());
