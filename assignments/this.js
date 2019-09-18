aac2b1adab9f088acf9f338b866991c8642d081a
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-09-18 10:41:36 -0700

    Finished this.js Principal 2

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding is the default state of the 'this' keyword, which uses the window object to try to satisfy the statment.
-* 2. 
+* 2. Implicit Binding is a binding where the 'this' keyword is implied via dot notation E.G. : adam.sayHi -> adam = this
 * 3. 
 * 4. 
 *
@@ -18,6 +18,11 @@ function howOld(){
 console.log(howOld());//undefined
 // Principle 2
 
+const person = {
+    name: "Bobby",
+    greeting: function(){ return `Yo its ${this.name}.`}
+}
+console.log(person.greeting());
 // code example for Implicit Binding
 
 // Principle 3
